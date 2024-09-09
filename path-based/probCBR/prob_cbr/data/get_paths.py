03a39fd2679087ef75a759b4289b5ba03e61d202
Roger Tu <rogertu@scripps.edu>
2024-09-09 15:28:14 -0700

    Update get_paths and add pr_cbr_mod 1. get_paths 	- fixed relative import 	- fix missing argument 2. pr_cbr_mod 	- initial commit 	- removed fallback since the linkage is default set to 0 	- add parameter for `create_adj_list` to allow inverse edge 	  creation 	- fix random self.arg references 	- fix subgraph name import 	- debug path_prior creation 	- debug precision_map creation
          - antlr4-python3-runtime==4.9.3

--- path-based/probCBR/prob_cbr/data/get_paths.py
+++ path-based/probCBR/prob_cbr/data/get_paths.py
@@ -1,21 +1,21 @@
-from tqdm import tqdm
-from collections import defaultdict
-import os
-import numpy as np
-from prob_cbr.data.data_utils import get_unique_entities, create_adj_list, get_inv_relation
-import time
-import pickle
 import argparse
 import json
-import wandb
 import logging
+import os
+import pickle
+import time
+from collections import defaultdict
 
-logger = logging.getLogger('get_paths')
+import numpy as np
+import wandb
+from data.data_utils import create_adj_list, get_inv_relation, get_unique_entities
+from tqdm import tqdm
+
+logger = logging.getLogger("get_paths")
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-formatter = logging.Formatter("[%(asctime)s \t %(message)s]",
-                              "%Y-%m-%d %H:%M:%S")
+formatter = logging.Formatter("[%(asctime)s \t %(message)s]", "%Y-%m-%d %H:%M:%S")
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
@@ -62,7 +62,7 @@ def main(args):
     out_dir = os.path.join(args.data_dir, "subgraphs", args.dataset_name)
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    args.prevent_loops = (args.prevent_loops == 1)
+    args.prevent_loops = args.prevent_loops == 1
     logger.info(json.dumps(vars(args), indent=4, sort_keys=True))
 
     kg_file = os.path.join(data_dir, "graph.txt")
@@ -78,9 +78,13 @@ def main(args):
         if args.use_wandb and ctr % 100 == 0:
             wandb.log({"progress": ctr / len(unique_entities)})
 
-    logger.info("Took {} seconds to collect paths for {} entities".format(time.time() - st_time, len(paths_map)))
+    logger.info(
+        "Took {} seconds to collect paths for {} entities".format(
+            time.time() - st_time, len(paths_map)
+        )
+    )
 
-    out_file_name = "paths_"+str(args.num_paths_to_collect)
+    out_file_name = "paths_" + str(args.num_paths_to_collect)
     if args.prevent_loops:
         out_file_name += "_no_loops"
     out_file_name += ".pkl"
@@ -92,17 +96,29 @@ def main(args):
     fout.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Collect subgraphs around entities")
     parser.add_argument("--dataset_name", type=str, default="nell")
-    parser.add_argument("--data_dir", type=str, default="/mnt/nfs/scratch1/rajarshi/deep_case_based_reasoning/")
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        default="/mnt/nfs/scratch1/rajarshi/deep_case_based_reasoning/",
+    )
     parser.add_argument("--num_paths_to_collect", type=int, default=1000)
-    parser.add_argument("--prevent_loops", type=int, choices=[0, 1], default=1, help="prevent sampling of looped paths")
+    parser.add_argument(
+        "--prevent_loops",
+        type=int,
+        choices=[0, 1],
+        default=1,
+        help="prevent sampling of looped paths",
+    )
     parser.add_argument("--add_inv_edges", action="store_true")
-    parser.add_argument("--use_wandb", type=int, choices=[0, 1], default=0, help="Set to 1 if using W&B")
+    parser.add_argument(
+        "--use_wandb", type=int, choices=[0, 1], default=0, help="Set to 1 if using W&B"
+    )
     args = parser.parse_args()
 
     if args.use_wandb:
-        wandb.init(project='collect-paths')
+        wandb.init(project="collect-paths")
 
     main(args)
